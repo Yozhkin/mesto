6826607a4a65108a54d1923c37fba883f9f66bbc
Alexey Puzyrev <yozh1982@gmail.com>
2023-01-24 18:47:28 +0300

    ПР - 4 (исправить замечания rw2)

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,28 @@
 const popupElement = document.querySelector('.popup');
 const popupButtonCloseElement = popupElement.querySelector('.popup__button-close');
 const popupButtonEditElement = document.querySelector('.profile__edit-button');
-const popupButtonSaveElement = popupElement.querySelector('popup__button-save');
-let popupInputNameElement = popupElement.querySelector('.popup__input-name');
-let popupInputJobElement = popupElement.querySelector('.popup__input-job');
-let formElement = popupElement.querySelector('.popup__input');
+let popupInputNameElement = popupElement.querySelector('.popup__input_name');
+let popupInputJobElement = popupElement.querySelector('.popup__input_job');
+let formElement = popupElement.querySelector('.popup__input_block');
 let profileNameElement = document.querySelector('.profile__user-name');
 let profileJobElement = document.querySelector('.profile__about-user');
-const likeAddedElements = document.querySelectorAll('.elements__like');
+// const likeAddedElements = document.querySelectorAll('.elements__like');
 
-for (let i = 0; i < likeAddedElements.length; i++) {
-  const likeAddedElement = likeAddedElements[i];
-  likeAddedElement.addEventListener("click", addLike);
-}
+// Функция кнопки like
 
-function addLike(event)
-{
-  for (let i = 0; i < likeAddedElements.length; i++) {
-  }
-  event.target.classList.toggle('elemetns__like_added');
-}
+// for (let i = 0; i < likeAddedElements.length; i++) {
+//   const likeAddedElement = likeAddedElements[i];
+//   likeAddedElement.addEventListener("click", addLike);
+// }
 
+// function addLike(event)
+// {
+//   for (let i = 0; i < likeAddedElements.length; i++) {
+//   }
+//   event.target.classList.toggle('elemetns__like_added');
+// }
+
+// Функция открытия popup
 function openPopup() {
   popupElement.classList.add('popup_opened');
 };
@@ -29,24 +31,23 @@ function closePopup() {
   popupElement.classList.remove('popup_opened');
 };
 
-function closePopupByClickOverlay(event) {
-  if (event.target !== event.currentTarget) {
-  return;
-}
-  closePopup();
-};
+// Фунция зарытия popup overlay
+// function closePopupByClickOverlay(event) {
+//   if (event.target !== event.currentTarget) {
+//   return;
+// }
+//   closePopup();
+// };
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileNameElement.textContent = popupInputNameElement.value;
   profileJobElement.textContent = popupInputJobElement.value;
-  popupInputNameElement.value = '';
-  popupInputJobElement.value = '';
   closePopup();
 };
 
 popupButtonEditElement.addEventListener('click', openPopup);
 popupButtonCloseElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOverlay);
+// popupElement.addEventListener('click', closePopupByClickOverlay);
 formElement.addEventListener('submit', handleFormSubmit);
 
