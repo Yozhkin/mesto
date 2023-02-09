e6d849af8b204ea7bb29afeaa99ed6b0c0bbbac4
Alexey Puzyrev <yozh1982@gmail.com>
2023-02-09 23:17:41 +0300

    add like btn & delete btn on cards

--- scripts/index.js
+++ scripts/index.js
@@ -33,59 +33,105 @@ let popupInputJobElement = popupElement.querySelector('.popup__input_type_job');
 let formElement = popupElement.querySelector('.popup__form');
 let profileNameElement = document.querySelector('.profile__user-name');
 let profileJobElement = document.querySelector('.profile__about-user');
-const likeAddedElements = document.querySelectorAll('.elements__like');
+
+const cardsContainer = document.querySelector('.elements__container');
 const templateCard = document.querySelector('#template-cards').content;
-const cardsBlock = document.querySelector('.elements__cards');
 
-const createCard = function(item) {
+function createCard(data) {
   const card = templateCard.cloneNode(true);
-  card.querySelector('.elements__title').textContent = item.name;
-  card.querySelector('.elements__foto').src = item.link;
+  const cardTitle = card.querySelector('.elements__title');
+  const cardFoto = card.querySelector('.elements__foto');
+  cardTitle.textContent = data.name;
+  cardFoto.alt = data.name;
+  cardFoto.src = data.link;
 
-  const like = card.querySelector('.elements__like');
-    like.addEventListener('click', function(event) {
-    event.target.classList.toggle('elemetns__like_added'); });
+  const deleteBtn = card.querySelector('.elements__del-btn');
+    deleteBtn.onclick = function(evt) {
+      evt.target.closest('.elements__card').remove();
+    };
+
+  const likeBtn = card.querySelector('.elements__like');
+    likeBtn.onclick = function(evt) {
+      evt.target.classList.toggle('elemetns__like_added');
+    };
   return card;
 };
 
-const renderCard = function(item) {
-  cardsBlock.append(createCard(item));
-};
+function renderCard(data) {
+  const cardElement = createCard(data);
+  cardsContainer.prepend(cardElement);
+}
 
- initialCards.forEach(function(item) {
-   renderCard(item);
-   });
+initialCards.forEach(function(card) {
+  renderCard(card);
+});
 
-// Функция открытия popup
-function openPopup() {
-  popupInputNameElement.value = profileNameElement.textContent;
-  popupInputJobElement.value = profileJobElement.textContent;
-  popupElement.classList.add('popup_opened');
-};
 
-function closePopup() {
-  popupElement.classList.remove('popup_opened');
-};
 
-// Фунция зарытия popup overlay
 
-// function closePopupByClickOverlay(event) {
-//   if (event.target !== event.currentTarget) {
-//   return;
-// }
+
+
+
+
+
+
+
+
+
+// const createCard = function(item) {
+//   const card = templateCard.cloneNode(true);
+//   card.querySelector('.elements__title').textContent = item.name;
+//   card.querySelector('.elements__foto').src = item.link;
+//
+//   delBtnElement.addEventListener('click', function(event) {
+//     event.target.closest('.elements__card');
+//     delBtnElement.remove()
+//   });
+//
+
+//
+//   return card;
+// };
+//
+// const renderCard = function(item) {
+//   cardsBlock.append(createCard(item));
+// };
+//
+//  initialCards.forEach(function(item) {
+//    renderCard(item);
+//    });
+//
+// // Функция открытия popup
+// function openPopup() {
+//   popupInputNameElement.value = profileNameElement.textContent;
+//   popupInputJobElement.value = profileJobElement.textContent;
+//   popupElement.classList.add('popup_opened');
+// };
+//
+// function closePopup() {
+//   popupElement.classList.remove('popup_opened');
+// };
+//
+// // Фунция зарытия popup overlay
+//
+// // function closePopupByClickOverlay(event) {
+// //   if (event.target !== event.currentTarget) {
+// //   return;
+// // }
+// //   closePopup();
+// // };
+//
+// function handleFormSubmit(evt) {
+//   evt.preventDefault();
+//   profileNameElement.textContent = popupInputNameElement.value;
+//   profileJobElement.textContent = popupInputJobElement.value;
 //   closePopup();
 // };
-
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-  profileNameElement.textContent = popupInputNameElement.value;
-  profileJobElement.textContent = popupInputJobElement.value;
-  closePopup();
-};
-
-
-popupButtonEditElement.addEventListener('click', openPopup);
-popupButtonCloseElement.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
-
-// popupElement.addEventListener('click', closePopupByClickOverlay);
+//
+//
+// popupButtonEditElement.addEventListener('click', openPopup);
+// popupButtonCloseElement.addEventListener('click', closePopup);
+// formElement.addEventListener('submit', handleFormSubmit);
+//
+// // popupElement.addEventListener('click', closePopupByClickOverlay);
+//
