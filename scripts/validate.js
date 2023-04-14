b08803b4c90abb2451eff1241d176b0002f99638
Alexey Puzyrev <yozh1982@gmail.com>
2023-04-14 19:11:15 +0300

    #

--- scripts/validate.js
+++ scripts/validate.js
@@ -4,7 +4,7 @@ const validationConfig = {
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_message'
+  errorClass: 'popup__error-message'
 };
 
 function enableValidation({formSelector, ...rest}) {
